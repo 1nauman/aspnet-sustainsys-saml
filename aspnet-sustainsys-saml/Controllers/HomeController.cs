cccd59c49398a5970337f97f38cc9248ffc3fd4f
numan-r <nauman.rehman@gmail.com>
2018-11-08 17:34:02 +0530

    initial workign version

---
+++ aspnet-sustainsys-saml/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using System.Web.Mvc;
+
+namespace aspnet_sustainsys_saml.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Secure()
+        {
+            var identity = System.Web.HttpContext.Current.User.Identity as ClaimsIdentity;
+            return View(identity.Claims);
+        }
+    }
+}
---
+++ aspnet-sustainsys-saml/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="aspnet_sustainsys_saml.MvcApplication" Language="C#" %>
